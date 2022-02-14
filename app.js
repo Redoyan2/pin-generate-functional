31295bd97ac7fc1c6a21f1154e617c4e538898b5
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-14 07:14:49 +0600

    matched

--- app.js
+++ app.js
@@ -10,23 +10,42 @@ function getPin() {
 }
 
 
-
+const pinDisplay = document.getElementById('pin-display');
 function generatePin() {
     const pin = getPin();
-    const pinDisplay = document.getElementById('pin-display');
     pinDisplay.value = pin;
 }
 
 
-
+const keyDisplay = document.getElementById('key-display');
 document.getElementById('key-pad').addEventListener('click',
     function (event) {
         const keyNumber = event.target.innerText;
-        const keyDisplay = document.getElementById('key-display');
-        previousDisplay = keyDisplay.value;
-        currentDisplay = previousDisplay + keyNumber;
-        keyDisplay.value = currentDisplay;
+        if (isNaN(keyNumber)) {
+            if(keyNumber=='C'){
+                keyDisplay.value= '';
+            }
+        }
+        else {
+            previousDisplay = keyDisplay.value;
+            currentDisplay = previousDisplay + keyNumber;
+            keyDisplay.value = currentDisplay;
+        }
+
 
     })
 
 
+document.getElementById('submit-btn').addEventListener('click', function () {
+    console.log('clicked submit');
+    const notifyFailed = document.getElementById('notify-fail');
+    const notifySuccess = document.getElementById('notify-success');
+    if(pinDisplay.value==keyDisplay.value){
+        notifySuccess.style.display = 'block';
+        notifyFailed.style.display = 'none';
+    }
+    else{
+        notifyFailed.style.display = 'block';
+        notifySuccess.style.display = 'none';
+    }
+})

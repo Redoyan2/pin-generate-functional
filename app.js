41a462272a2cbed275f2c20fd79cabec01aa0fbe
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-14 06:52:05 +0600

    key display

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 
-function getPin(){
-    const pin = Math.round(Math.random()*10000);
-    if (pin>=1000){
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    if (pin >= 1000) {
         return pin;
     }
-    else{
+    else {
         return getPin();
     }
 }
 
 
 
-function generatePin(){
+function generatePin() {
     const pin = getPin();
     const pinDisplay = document.getElementById('pin-display');
     pinDisplay.value = pin;
 }
+
+
+
+document.getElementById('key-pad').addEventListener('click',
+    function (event) {
+        const keyNumber = event.target.innerText;
+        const keyDisplay = document.getElementById('key-display');
+        previousDisplay = keyDisplay.value;
+        currentDisplay = previousDisplay + keyNumber;
+        keyDisplay.value = currentDisplay;
+
+    })
+
 
